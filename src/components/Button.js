91c4c76024ad7a1d81453744d7de6d87d1fb6fd4
Treasure Kabareebe <treasurecalm@gmail.com>
2020-08-06 08:53:33 +0300

    Add wide and color props to button to change color and button width depending on props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../css/Button.css'
+import '../css/Button.css';
 
 const CalcButton = ({ name, color, wide }) => (
-  <button className='Button' type="submit">{name}</button>
+  <button
+    style={{ width: wide ? '50%' : '25%', backgroundColor: color }}
+    className="Button"
+    type="submit"
+  >
+    {name}
+  </button>
 );
 
 CalcButton.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
+
+CalcButton.defaultProps = {
+  color: '#F5923E',
+  wide: false,
 };
 
 export default CalcButton;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,33 +4,33 @@ import '../css/ButtonPanel.css';
 
 const ButtonPanel = () => (
   <div className="ButtonPanel">
-    <div className='ButtonPanelGroup'>
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
+    <div className="ButtonPanelGroup">
+      <Button name="AC" color="#E0E0E0" />
+      <Button name="+/-" color="#E0E0E0" />
+      <Button name="%" color="#E0E0E0" />
       <Button name="/" />
     </div>
-    <div className='ButtonPanelGroup'>
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
+    <div className="ButtonPanelGroup">
+      <Button name="7" color="#E0E0E0" />
+      <Button name="8" color="#E0E0E0" />
+      <Button name="9" color="#E0E0E0" />
       <Button name="*" />
     </div>
-    <div className='ButtonPanelGroup'>
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
+    <div className="ButtonPanelGroup">
+      <Button name="4" color="#E0E0E0" />
+      <Button name="5" color="#E0E0E0" />
+      <Button name="6" color="#E0E0E0" />
       <Button name="-" />
     </div>
-    <div className='ButtonPanelGroup'>
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
+    <div className="ButtonPanelGroup">
+      <Button name="1" color="#E0E0E0" />
+      <Button name="2" color="#E0E0E0" />
+      <Button name="3" color="#E0E0E0" />
       <Button name="+" />
     </div>
-    <div className='ButtonPanelGroup'>
-      <Button name="0" />
-      <Button name="." />
+    <div className="ButtonPanelGroup">
+      <Button name="0" wide color="#E0E0E0" />
+      <Button name="." color="#E0E0E0" />
       <Button name="=" />
     </div>
   </div>
