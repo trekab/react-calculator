48644355cc3bf960c9f4900b58cd8bc52eab9fe6
Treasure Kabareebe <treasurecalm@gmail.com>
2021-01-26 22:13:24 +0300

    chore: add the css and logic folders with the code as before

---
+++ src/logic/operate.js
@@ -0,0 +1,31 @@
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  let result = null;
+  const firstNumber = new Big(numberOne);
+  const secondNumber = new Big(numberTwo);
+
+  switch (operation) {
+    case '+':
+      result = (firstNumber.plus(secondNumber)).toString();
+      break;
+    case '-':
+      result = (firstNumber.minus(secondNumber)).toString();
+      break;
+    case '*':
+      result = (firstNumber.times(secondNumber)).toString();
+      break;
+    case '/':
+      if (numberTwo === '0') {
+        result = 'Division by zero is not possible.';
+      } else {
+        result = (firstNumber.div(secondNumber)).toString();
+      }
+      break;
+    default:
+      break;
+  }
+  return result;
+};
+
+export default operate;
